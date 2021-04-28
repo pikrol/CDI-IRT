97cecefc32f05abae9f4f788e61baf25f9a9a8ba
pikrol <pkrol4478@gmail.com>
2021-04-28 11:23:19 +0200

    Investigate not fitting items.

--- 2Model.R
+++ 2Model.R
@@ -30,7 +30,7 @@ fullThetas <- data.frame(KOD = responsesDemo$KOD, fullTheta = fullThetas)
 colnames(fullThetas) <- c("KOD", "fullTheta")
 # write.csv(fullThetas, file = "Data/fullThetas.csv", fileEncoding = "utf-8", row.names = F)
 
-png("Plots/PointsVsTheta.png", width = 853, pointsize = 16)
+png("Plots/pointsVsTheta.png", width = 853, pointsize = 16)
 plot(data.frame(Score = rowSums(responses), Theta = fullThetas), xlab = "Points", ylab = "Theta", main = "Relation between theta and points")
 dev.off()
 
@@ -45,10 +45,10 @@ dev.off()
 # write.csv(itemsFit, file = "Data/itemsFit.csv", fileEncoding = "utf-8", row.names = F)
 
 #Load saved items fit
-itemsFit <- read.csv("Data/itemsFit.csv", encoding = "UTF-8")
+# itemsFit <- read.csv("Data/itemsFit.csv", encoding = "UTF-8")
 
 #Check number of not fitting items (p < 0.05) - 35
-nrow(itemsFit[itemsFit$p.S_X2 < 0.05, ])
+# nrow(itemsFit[itemsFit$p.S_X2 < 0.05, ])
 
 #Order by p-values
 # itemsFit <- itemsFit[order(itemsFit$p.S_X2), ]
@@ -57,26 +57,51 @@ nrow(itemsFit[itemsFit$p.S_X2 < 0.05, ])
 # itemplot(model, 652)
 
 #Get numbers of not fitting items
-notFitting <- itemsFit[itemsFit$p.S_X2 < 0.05, "item"]
-save(notFitting, file = "Data/notFitting")
+# notFitting <- itemsFit[itemsFit$p.S_X2 < 0.05, "item"]
+# save(notFitting, file = "Data/notFitting")
+
+#Load saved vector with not fitting items numbers
+load("Data/notFitting")
+notFittingItems <- cdi[is.element(cdi$Numer, notFitting), ]
 
 #----------------
 #ITEMS PARAMETERS
 #----------------
 
 #Obtain items parameters from model
-l <- coef(model)
-l$GroupPars <- NULL
-params <- data.frame(matrix(unlist(l), nrow=670, byrow=T))[1:2]
-colnames(params) <- c("a1", "d")
-params$Numer <- names(l)
-params <- merge(cdi, params, by = "Numer")
-params <- params[,c("Numer", "NumerSiG", "Pozycja", "a1", "d", "Kategoria")]
-params <- params[order(as.integer(params$Numer)), ]
-rownames(params) <- NULL
+# l <- coef(model)
+# l$GroupPars <- NULL
+# params <- data.frame(matrix(unlist(l), nrow=670, byrow=T))[1:2]
+# colnames(params) <- c("a1", "d")
+# params$Numer <- names(l)
+# params <- merge(cdi, params, by = "Numer")
+# params <- params[,c("Numer", "NumerSiG", "Pozycja", "a1", "d", "Kategoria")]
+# params <- params[order(as.integer(params$Numer)), ]
+# rownames(params) <- NULL
 # write.csv(params, file = "Data/params.csv", fileEncoding = "utf-8", row.names = F)
 
 #Save items params plot
-png("Plots/params.png", width = 853, pointsize = 16)
-plot(params$d, params$a1, xlab = "Difficulty", ylab = "Discrimination", main = "Items parameters")
-dev.off()
+# png("Plots/params.png", width = 853, pointsize = 16)
+# plot(params$d, params$a1, xlab = "Difficulty", ylab = "Discrimination", main = "Items parameters")
+# dev.off()
+
+params <- read.csv("Data/params.csv", encoding = "UTF-8")
+
+#-----------------
+#NOT FITTING ITEMS
+#-----------------
+
+#Prepare df with not fitting items and their params
+notFittingItems$Discrimination <- params[is.element(params$Numer, notFittingItems$Numer), "a1"]
+notFittingItems$Difficulty <- params[is.element(params$Numer, notFittingItems$Numer), "d"]
+write.csv(notFittingItems, file = "Results/notFittingItems.csv", fileEncoding = "utf-8", row.names = F)
+
+notFittingCategories <- sort(table(notFittingItems$Kategoria), decreasing = TRUE)
+write.table(notFittingCategories, file = "Results/notFittingCategories.txt", sep = "\t", col.names = FALSE, row.names = FALSE)
+
+#Check not fitting items params
+# notFittingItems$DscrmnNormal <- ifelse(notFittingItems$Discrimination > mean(params$a1) - sd(params$a1) & notFittingItems$Discrimination < mean(params$a1) + sd(params$a1), 1, 0)
+# 
+# notFittingItems$DiffNormal <- ifelse(notFittingItems$Difficulty > mean(params$d) - sd(params$d) & notFittingItems$Difficulty < mean(params$d) + sd(params$d), 1, 0)
+# 
+# table(notFittingItems$DscrmnNormal)
